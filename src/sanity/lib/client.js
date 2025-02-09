6476c082ac3b7b1ea987311c8371c9e9fbb73a19
hmweb77 <oussamahamdani1@gmail.com>
2025-02-09 09:39:42 +0100

    fixing issue in next config and createClient 2

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  projectId: "pkg5i4cw",
   dataset: 'production',
-  apiVersion,
+  apiVersion:'2023-10-01',
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
