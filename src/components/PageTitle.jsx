27390fa956a563007288c39bcc324ce622bb264d
hmweb77 <oussamahamdani1@gmail.com>
2025-03-05 17:36:37 +0000

    changing pageTitle

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -13,8 +13,8 @@ const PageTitle = ({title}) => {
             className="cursor-pointer absolute left-0 top-1/2 -translate-y-1/2 text-black"
             size={40}
           />
-          <h1 className="relative text-5xl font-black mb-2 tracking-wide">
-            <span className="absolute -right-1 text-[#4AE54A] z-0">{title}</span>
+          <h1 className="relative ml-8 text-center sm:text-5xl text-4xl font-black mb-2 tracking-wide">
+            <span className="absolute -left-1 text-[#4AE54A] z-0">{title}</span>
             <span className="relative text-black z-10">{title}</span>
           </h1>
         </div>
