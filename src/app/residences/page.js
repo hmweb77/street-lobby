2672d7dc0dd362353ac43b3d510be4df4a85081b
hmweb77 <oussamahamdani1@gmail.com>
2025-08-08 21:58:02 +0100

    updating license

--- src/app/residences/page.js
+++ src/app/residences/page.js
@@ -79,6 +79,7 @@ export default function PropertyMap() {
             coordinates,
             descriptions,
             zipCode,
+            alLicense,
             city
           },
           slug
